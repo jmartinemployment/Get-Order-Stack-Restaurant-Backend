3857c58c9dfa43aee5a6a9a37240a23d054474d5
Jeff Martin <jmartinpersonal@yahoo.com>
2026-01-21 08:26:40 -0500

    Fix Stripe API version for deployment

--- src/services/stripe.service.ts
+++ src/services/stripe.service.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2025-01-27.acacia'
+  apiVersion: '2025-12-15.clover'
 });
 
 export interface CreatePaymentIntentParams {
